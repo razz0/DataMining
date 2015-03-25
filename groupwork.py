ea5bbba5902d5565d20421800c034a7ca71a3ec7
Mikko Koho <mikko.koho@csc.fi>
2015-03-25 17:05:13 +0200

    Finetune course renewal timespans

--- groupwork.py
+++ groupwork.py
@@ -2,16 +2,18 @@ import students as s
 
 s.read_students()
 
-OLD_TIME = ('2005-08', '2010-01')
-NEW_TIME = ('2010-02', '2020-01')
+COURSESET_TIMESPANS = [{'old': ('2005-08', '2010-01'), 'new': ('2010-02', '2020-01')},
+                       {'old': ('2005-08', '2010-04'), 'new': ('2010-05', '2020-01')},
+                       {'old': ('2005-08', '2010-01'), 'new': ('2010-05', '2020-01')},
+                       ]
 
-INTERESTING_COURSESETS = [s.OHJ_PER, s.OHJ_JAT, s.OHJ_PER | s.OHJ_JAT]
+INTERESTING_COURSESETS = [s.OHJ_PER, s.OHJ_JAT2, s.OHJ_PER | s.OHJ_JAT2]
 
-for courseset in INTERESTING_COURSESETS:
-    passed_old = s.filter_students_by_courses(s.students, courseset, grades=s.PASSED_GRADES, timespan=OLD_TIME)
-    failed_old = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=OLD_TIME)
-    passed_new = s.filter_students_by_courses(s.students, courseset, grades=s.PASSED_GRADES, timespan=NEW_TIME)
-    failed_new = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=NEW_TIME)
+for courseset, timespan in zip(INTERESTING_COURSESETS, COURSESET_TIMESPANS):
+    passed_old = s.filter_students_by_courses(s.students, courseset, grades=s.PASSED_GRADES, timespan=timespan['old'])
+    failed_old = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=timespan['old'])
+    passed_new = s.filter_students_by_courses(s.students, courseset, grades=s.PASSED_GRADES, timespan=timespan['new'])
+    failed_new = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=timespan['new'])
 
     print 'Percentage of participants passing old %s: %s' % \
           (list(courseset), len(passed_old) / float(len(passed_old | failed_old)))
@@ -20,14 +22,15 @@ for courseset in INTERESTING_COURSESETS:
 
 print
 
+
 def _compare_time(x, y):
     return cmp(x['time'], y['time'])
 
-for courseset in [s.OHJ_PER, s.OHJ_JAT2]:
+for courseset, timespan in zip([s.OHJ_PER, s.OHJ_JAT2], COURSESET_TIMESPANS[:2]):
     coursename = list(courseset)[0] if len(courseset) == 1 else list(courseset)
 
-    failed_old = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=OLD_TIME)
-    failed_new = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=NEW_TIME)
+    failed_old = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=timespan['old'])
+    failed_new = s.filter_students_by_courses(s.students, courseset, grades=s.FAILED_GRADES, timespan=timespan['new'])
 
     passed_old_grades = []
     passed_new_grades = []
@@ -37,14 +40,14 @@ for courseset in [s.OHJ_PER, s.OHJ_JAT2]:
     retries_new_new = []
 
     for stud in failed_old:
-        failed_attempts = stud.get_course_by_name(coursename, grades=s.FAILED_GRADES, timespan=OLD_TIME)
+        failed_attempts = stud.get_course_by_name(coursename, grades=s.FAILED_GRADES, timespan=timespan['old'])
         fail_times = sorted([fail['time'] for fail in failed_attempts])
 
         passed_old_attempts = sorted(
-            stud.get_course_by_name(coursename, grades=s.PASSED_GRADES, timespan=(fail_times[0], OLD_TIME[1])),
+            stud.get_course_by_name(coursename, grades=s.PASSED_GRADES, timespan=(fail_times[0], timespan['old'][1])),
             cmp=_compare_time)
         passed_new_attempts = sorted(
-            stud.get_course_by_name(coursename, grades=s.PASSED_GRADES, timespan=NEW_TIME), cmp=_compare_time)
+            stud.get_course_by_name(coursename, grades=s.PASSED_GRADES, timespan=timespan['new']), cmp=_compare_time)
 
         if passed_old_attempts:
             passed_old_grades.append([int(c['grade']) for c in passed_old_attempts][0])
@@ -63,10 +66,10 @@ for courseset in [s.OHJ_PER, s.OHJ_JAT2]:
     passed_new_grades = []
 
     for stud in failed_new:
-        failed_attempts = stud.get_course_by_name(coursename, grades=s.FAILED_GRADES, timespan=NEW_TIME)
+        failed_attempts = stud.get_course_by_name(coursename, grades=s.FAILED_GRADES, timespan=timespan['new'])
         first_fail = min([fail['time'] for fail in failed_attempts])
 
-        passed_new_attempts = stud.get_course_by_name(coursename, grades=s.PASSED_GRADES, timespan=(first_fail, NEW_TIME[1]))
+        passed_new_attempts = stud.get_course_by_name(coursename, grades=s.PASSED_GRADES, timespan=(first_fail, timespan['new'][1]))
 
         if passed_new_attempts:
             passed_new_grades.append([int(c['grade']) for c in
