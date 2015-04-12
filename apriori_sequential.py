363258c36bfa5756f9760fa0de12c0ce4dd638eb
Mikko Koho <mikko.koho@csc.fi>
2015-04-12 15:48:28 +0300

    Sequential apriori working

--- apriori_sequential.py
+++ apriori_sequential.py
@@ -4,6 +4,7 @@ Model sequences like ((1, 2, 3), (4, 5), (4, 6)).
 """
 
 from collections import defaultdict
+from pprint import pprint
 import copy
 import itertools
 
@@ -13,14 +14,14 @@ def flatten(sequence):
     return [event for element in sequence for event in element]
 
 
-def subsequence(seq1, seq2):
+def is_subsequence(seq1, seq2):
     """Check if seq1 is a subsequence of seq2
 
-    >>> subsequence(((2,), (3, 5)), ((2, 4), (3, 5, 6), (8,)))
+    >>> is_subsequence(((2,), (3, 5)), ((2, 4), (3, 5, 6), (8,)))
     True
-    >>> subsequence(((1,), (2,)), ((1, 2), (3, 4)))
+    >>> is_subsequence(((1,), (2,)), ((1, 2), (3, 4)))
     False
-    >>> subsequence(((2,), (4,)), ((2, 4), (2, 4), (2, 5)))
+    >>> is_subsequence(((2,), (4,)), ((2, 4), (2, 4), (2, 5)))
     True
     """
     seq = copy.deepcopy(seq1)
@@ -42,7 +43,7 @@ def support_count(sequence, seq_list):
     >>> [support_count(((item,),), simple_seqs) for item in range(1, 5)]
     [1, 3, 2, 2]
     """
-    return len([seq for seq in seq_list if subsequence(sequence, seq)])
+    return len([seq for seq in seq_list if is_subsequence(sequence, seq)])
 
 
 def _sequential_candidate_generation(sequences, k):
@@ -55,63 +56,69 @@ def _sequential_candidate_generation(sequences, k):
     >>> _sequential_candidate_generation([(('A',),), (('B',),), (('C',),)], 2)
     [(('A',), ('B',)), (('A', 'B'),), (('A',), ('C',)), (('A', 'C'),), (('B',), ('A',)), (('B',), ('C',)), (('B', 'C'),), (('C',), ('A',)), (('C',), ('B',))]
     >>> _sequential_candidate_generation([(('A', 'B'),), (('A', 'C'),), (('B',), ('C',))], 3)
-    [('A', 'B', 'C')]
+    [(('A', 'B'), ('C',))]
     >>> _sequential_candidate_generation([(('A',), ('B',)), (('A', 'C'),), (('B', 'C'),), (('C', 'C'),)], 3)
     [(('A',), ('B', 'C')), (('A', 'C', 'C'),), (('B', 'C', 'C'),)]
+    >>> _sequential_candidate_generation([((1,),), ((2,),), ((3,),)], 2)
+    [((1,), (1,)), ((1,), (2,)), ((1, 2),), ((1,), (3,)), ((1, 3),), ((2,), (1,)), ((2,), (2,)), ((2,), (3,)), ((2, 3),), ((3,), (1,)), ((3,), (2,)), ((3,), (3,))]
+    >>> _sequential_candidate_generation([((1,), (2,)), ((2,), (3,))], 3)
+    [((1,), (2,), (3,))]
     """
 
     new_candidates = []
     for index1, seq1 in enumerate(sequences):
         for index2, seq2 in enumerate(sequences):
-            if index1 == index2:
-                continue
-            seq1_flattened = flatten(seq1)
-            seq2_flattened = flatten(seq2)
             if k == 2:
                 # Assume we get 1-sequences like we should
                 new_candidates.append((seq1[0], seq2[0],))
                 if seq1[0] < seq2[0]:
                     new_candidates.append(((seq1[0] + seq2[0]),))
             elif k > 2:
+                seq1_flattened = flatten(seq1)
+                seq2_flattened = flatten(seq2)
+                if index1 == index2:
+                    continue
                 if seq1_flattened[1:] == seq2_flattened[:-1]:
                     new_sequence = copy.deepcopy(seq1)
                     if len(seq2[-1]) > 1:
                         new_sequence = new_sequence[:-1] + (new_sequence[-1] + (seq2_flattened[-1],),)
                     else:
-                        new_sequence += (seq2_flattened[-1][-1],)
+                        new_sequence += (seq2[-1],)
                     new_candidates.append(new_sequence)
 
     return new_candidates
 
 
-def transaction_subsets(transaction, k):
+def get_subsequences(sequence):
     """
-    Get subsets of transactions of length k
+    Get length k-1 subsequences of length k sequence
 
-    >>> transaction_subsets(['A', 'B', 'C', 'D', 'E'], 4)
-    [('A', 'B', 'C', 'D'), ('A', 'B', 'C', 'E'), ('A', 'B', 'D', 'E'), ('A', 'C', 'D', 'E'), ('B', 'C', 'D', 'E')]
+    >>> get_subsequences((('A', 'B'), ('C',)))
+    [(('A', 'B'),), (('A',), ('C',)), (('B',), ('C',))]
+    >>> get_subsequences((('A', 'B'), ('C',), ('D', 'E')))
+    [(('A', 'B'), ('C',), ('D',)), (('A', 'B'), ('C',), ('E',)), (('A', 'B'), ('D', 'E')), (('A',), ('C',), ('D', 'E')), (('B',), ('C',), ('D', 'E'))]
 
-    :param transaction: list
-    :param k: int
+    :rtype : tuple
     :return:
     """
-    subsets = []
+    subseqs = []
 
-    if k == 1:
-        return [(t,) for t in transaction]
+    for i in reversed(range(0, len(sequence))):
+        element = sequence[i]
+        for j in reversed(range(0, len(element))):
+            event = element[j]
+            if len(element) == 1:
+                subseq = sequence[:i] + sequence[(i + 1):]
+            else:
+                subseq = list(sequence)
+                subseq[i] = subseq[i][:j] + subseq[i][(j + 1):]
 
-    elif k > len(transaction):
-        return []
+            subseqs.append(tuple(subseq))
 
-    for i in range(0, len(transaction) - (k - 1)):
-        for t in transaction_subsets(transaction[i + 1:], k - 1):
-            subset = (transaction[i],) + t
-            subsets.append(subset)
-
-    return subsets
+    return subseqs
 
 
-def apriori_sequential(sequences, sequence_list, minsup, fixed_k=None, verbose=False):
+def apriori_sequential(sequences, minsup, fixed_k=None, verbose=False):
     """
     Apriori method for sequential patterns
 
@@ -119,22 +126,13 @@ def apriori_sequential(sequences, sequence_list, minsup, fixed_k=None, verbose=F
     :param all_items: list distinct items
     :param minsup: minimum support
 
-    >>> simple_sequences = [(('007', '666', '777'), ('007', 'BC',), ('007', '666'), ('777',)), \
-                            ((), ('007',), ('666',), ('BC',))]
-    >>> alphabet = ['007', '666', '777', 'BC']
-    >>> apriori_sequential(simple_sequences, alphabet, 0.3)
-    [('007',), ('666',), ('777',), ('007', '666')]
-    >>> apriori_sequential(simple_sequences, alphabet, 0.6)
-    [('007',)]
-    >>> apriori_sequential(simple_sequences, alphabet, 0.5, fixed_k=2)
-    [('007', '666')]
-    >>> apriori_sequential(simple_sequences, alphabet, 0.75)
-    [('007',)]
-    >>> apriori_sequential(simple_sequences, alphabet, 0.9)
-    []
+    >>> seqs = [((1, 2, 4), (2, 3), (5,)), \
+                ((1, 2), (2, 3, 4)), \
+                ((1, 2), (2, 3, 4), (2, 4, 5)), \
+                ((2,), (3, 4), (4, 5)), \
+                ((1, 3), (2, 4, 5))]
+    >>> apriori_sequential(seqs, 0.5)
     """
-
-    sequence_list = sorted(list(sequence_list))
 
     k = 1
     N = len(sequences)
@@ -142,41 +140,56 @@ def apriori_sequential(sequences, sequence_list, minsup, fixed_k=None, verbose=F
     frequent_sequences = [[], []]  # k index, zero always empty
     support = defaultdict(int)
 
-    for seq in sequence_list:
-        new_seq = (seq,)
-        support[new_seq] = support_count(new_seq, sequences)
+    for seq in sequences:
+        events = sorted(set(flatten(seq)))
+        for event in events:
+            event_seq = ((event,),)
+            support[event_seq] = support_count(event_seq, sequences)
 
-        if support[new_seq] >= N * minsup:
-            frequent_sequences[1].append(new_seq)
+            #print "k==1, event seq: %s - support: %s" % (event_seq, support[event_seq])
 
-    pruned_candidates = [True, 'dummy']
+            if support[event_seq] >= N * minsup and event_seq not in frequent_sequences[1]:
+                frequent_sequences[1].append(event_seq)
+
+    pruned_candidates = ['dummy', 'dummy']
+
+    #print frequent_sequences
 
     while pruned_candidates and len(pruned_candidates) > 1 and (not fixed_k or k < fixed_k):
-        candidate_sets = _sequential_candidate_generation(frequent_sequences[k], k)
         k += 1
+        candidate_seqs = _sequential_candidate_generation(frequent_sequences[k - 1], k)
         if verbose:
-            print 'k=%s - set count %s - support list length %s' % (k, len(candidate_sets), len(support))
-        if not candidate_sets:
+            print 'k=%s - seq count %s - support list length %s' % (k, len(candidate_seqs), len(support))
+        if not candidate_seqs:
             break
 
-        candidate_sets_as_set = set(candidate_sets)
+        pruned_candidates = []
 
-        for tindex, t in enumerate(sequences):
-            if verbose and k > 3 and tindex % (len(sequences) / (100 * (k - 3))) == 0:
-                print 'Transaction %s / %s' % (tindex, len(sequences))
-            subsets = transaction_subsets(t, k)
+        for can_seq in candidate_seqs:
+            subseqs = get_subsequences(can_seq)
+            if all([subseq in frequent_sequences[k - 1] for subseq in subseqs]):
+                pruned_candidates.append(can_seq)
 
-            for subset in subsets:
-                if set([subset]) <= candidate_sets_as_set:
-                    support[subset] += 1
+        for pruned_seq in pruned_candidates:
+            for seq_index, seq in enumerate(sequences):
+                if verbose and k > 3 and len(pruned_candidates) > 50 \
+                        and seq_index % (1 + len(sequences) / (100 * (k - 3))) == 0:
+                    print 'Sequence %s / %s' % (seq_index, len(sequences))
+
+                if is_subsequence(pruned_seq, seq):
+                    support[pruned_seq] += 1
 
         # Free up some memory
-        for key in [removable for removable in support.iterkeys() if len(removable) < k]:
-            del support[key]
+        #for key in [removable for removable in support.iterkeys() if len(removable) < k]:
+        #    del support[key]
 
-        pruned_candidates = [seq for seq in candidate_sets if support[seq] >= N * minsup]
+        #pruned_candidates = [seq for seq in candidate_seqs if support[seq] >= N * minsup]
 
-        frequent_sequences.append(pruned_candidates)
+        #frequent_sequences.append(pruned_candidates)
+
+        frequent_sequences.append([seq for seq in pruned_candidates if support[seq] >= N * minsup])
+
+        #print frequent_sequences[k]
 
     if fixed_k:
         try:
